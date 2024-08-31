99e4aab77fabea1cba8f7fbd6be6dac827dffc20
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-08-31 16:42:31 +0600

    free drink if else practic

---
+++ freedrinks.js
@@ -0,0 +1,14 @@
+/**
+ * Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+ */
+
+var burgerPrice = 500;
+
+if(burgerPrice >=500){
+    console.log("You get a free drink")
+}
+else{
+    console.log("You have to pay 30 taka for a coke")
+}
